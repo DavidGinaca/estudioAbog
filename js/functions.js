3680bdb6d29207b865606ef61071120aa46a3344
David Emiliano Ginaca <davidginaca@gmail.com>
2023-11-27 14:48:22 -0300

    primer commit

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+let cambio = document.getElementById('a1');
+let logo= document.getElementById('moon');
+
+cambio.addEventListener('change', (event)=>{
+    let chekeado = event.target.checked;
+    document.body.classList.toggle('darkM');
+
+    if(chekeado){
+        logo.innerHTML='<i class="fa-regular fa-sun"></i>';
+        logo.style.color='orange';
+    }else{
+        logo.innerHTML= '<i class="fa-regular fa-moon"></i>';
+        logo.style.color='grey';
+    }
+
+})
